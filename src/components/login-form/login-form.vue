a364f9e69b4e49dcd44b1835bf341aaf948e091a
brian <brian@toimc.com>
2020-02-23 14:36:14 +0800

    添加图片验证码样式

--- src/components/login-form/login-form.vue
+++ src/components/login-form/login-form.vue
@@ -1,5 +1,10 @@
 <template>
-  <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="handleSubmit">
+  <Form
+    ref="loginForm"
+    :model="form"
+    :rules="rules"
+    @keydown.enter.native="handleSubmit"
+  >
     <FormItem prop="userName">
       <Input v-model="form.userName" placeholder="请输入用户名">
         <span slot="prepend">
@@ -14,37 +19,54 @@
         </span>
       </Input>
     </FormItem>
+    <FormItem prop="code">
+      <Input
+        class="imooc-input"
+        type="password"
+        v-model="form.code"
+        placeholder="请输入验证码"
+      >
+        <span slot="prepend">
+          <Icon :size="14" type="md-image"></Icon>
+        </span>
+        <span
+          class="imooc-code"
+          slot="append"
+          v-html="svg"
+          @click="_getCode()"
+        ></span>
+      </Input>
+    </FormItem>
     <FormItem>
       <Button @click="handleSubmit" type="primary" long>登录</Button>
     </FormItem>
   </Form>
 </template>
 <script>
+import axios from 'axios'
 export default {
   name: 'LoginForm',
   props: {
     userNameRules: {
       type: Array,
       default: () => {
-        return [
-          { required: true, message: '账号不能为空', trigger: 'blur' }
-        ]
+        return [{ required: true, message: '账号不能为空', trigger: 'blur' }]
       }
     },
     passwordRules: {
       type: Array,
       default: () => {
-        return [
-          { required: true, message: '密码不能为空', trigger: 'blur' }
-        ]
+        return [{ required: true, message: '密码不能为空', trigger: 'blur' }]
       }
     }
   },
   data () {
     return {
+      svg: '',
       form: {
-        userName: 'super_admin',
-        password: ''
+        userName: '',
+        password: '',
+        code: ''
       }
     }
   },
@@ -56,6 +78,9 @@ export default {
       }
     }
   },
+  mounted () {
+    this._getCode()
+  },
   methods: {
     handleSubmit () {
       this.$refs.loginForm.validate((valid) => {
@@ -66,7 +91,40 @@ export default {
           })
         }
       })
+    },
+    _getCode () {
+      axios
+        .get('http://localhost:3000/public/getCaptcha?sid=toimc')
+        .then((res) => {
+          const obj = res.data
+          console.log('TCL: _getCode -> res', res)
+          if (res.status === 200) {
+            this.svg = obj.data
+          }
+        })
     }
   }
 }
 </script>
+<style lang="scss">
+.imooc-input {
+  .ivu-input-group-append {
+    padding: 0;
+  }
+  .ivu-input-group-prepend {
+    width: 32px;
+  }
+}
+
+.imooc-code {
+  display: inline-block;
+  padding: 0;
+  height: 28px;
+  overflow: hidden;
+  svg {
+    width: 120px;
+    position: relative;
+    top: -12px;
+  }
+}
+</style>
